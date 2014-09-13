f96c2e3423f861d684fb743747a1910a4499d74a
Sage Imel <sage@sagenite.net>
2014-09-12 22:53:36 -0700

    Fixup regen_schedule.sh

--- _scripts/regen_schedule.sh
+++ _scripts/regen_schedule.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+cd -- "$(dirname -- "$0")"
+
 ./regen_schedule.py > ../schedule.html
 git add ../schedule.html
 git commit -m "Automated schedule update."
-git push
+git push origin master
